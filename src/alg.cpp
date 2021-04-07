b8978841b4e112f0aa5c04b3e302a6f447fd26bb
Анастасия Карова <76094090+stasanastas@users.noreply.github.com>
2021-04-07 15:15:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int countPairs1(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; i++)  {
     for (int j = 0; j < len - 1; j++)  {
       if (arr[j] > arr[j + 1]) {
-        x = arr[j];
+        int x = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = x;
       }
@@ -13,7 +13,7 @@ int countPairs1(int *arr, int len, int value) {
   int res = 0;
   for (int i = 0; i < len - 1; i++)  {
     for (int j = i + 1; j < len - 1; j++)  {
-      if (arr[i] + arr[j] == value) && (i != j)
+      if ((arr[i] + arr[j] == value) && (i != j))
         res++;
     }
   }
@@ -24,17 +24,18 @@ int countPairs2(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; i++)  {
     for (int j = 0; j < len - 1; j++)  {
       if (arr[j] > arr[j + 1]) {
-        x = arr[j];
+        int x = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = x;
       }
     }
   }
+  int res = 0;
   for (int j = len - 1; j >= 0; j--) {
     if (arr[j] <= value) {
       for (int i = 0; i < j; i++) {
         if (arr[i] + arr[j] <= value) {
-          if arr[i] + arr[j] = value)
+          if (arr[i] + arr[j] == value)
             res++;
         }
       }
@@ -47,7 +48,7 @@ int countPairs3(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; i++)  {
     for (int j = 0; j < len - 1; j++)  {
       if (arr[j] > arr[j + 1]) {
-        x = arr[j];
+        int x = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = x;
       }
